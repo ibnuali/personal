6d843e74108da5c968cd9a70c90aff170663dee7
Ibnu Ali <ibnu.mukhtarom@len-iot.io>
2024-12-15 21:42:02 +0700

    update resoultion home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,15 @@
 const Home = () => {
   return (
     <div className="flex items-center justify-between">
-      <div className="flex flex-col">
-        <span className="text-[90px] font-bold text-white">Hello I'm</span>
-        <span className="text-[90px] font-bold text-white">Ibnu Ali M</span>
-        <p className="text-lg font-extralight text-white w-1/2">
+      <div className="grid grid-cols-1 gap-4 lg:flex lg:flex-col ">
+        <span className="text-[40px] lg:text-[90px] font-bold text-white">Hello I'm</span>
+        <span className="text-[35px] lg:text-[90px] font-bold text-white">Ibnu Ali M</span>
+        <p className="text-lg lg:text-lg font-extralight text-white lg:w-1/2">
           I'm a passionate full-stack developer with expertise in Node.js and
           React.js, creating seamless and efficient web solutions from backend
           to frontend.
         </p>
-        <div className="flex gap-4 mt-8">
+        <div className="grid grid-cols-1 lg:flex gap-4 mt-8">
           <button className="ring-red-500 hover:bg-red-500 hover:text-white ring-1 text-white font-bold py-2 px-4 rounded-full">
             <a target="_blank" href="./docs/cv.pdf">Download CV</a>
           </button>
@@ -24,7 +24,7 @@ const Home = () => {
         </div>
       </div>
       <img
-        className="w-48 h-48 lg:w-96 lg:h-96 rounded-full"
+        className="hidden md:block w-48 h-48 xl:w-96 xl:h-96 rounded-full"
         alt="Ibnu Ali"
         src="./avatar.jpg"
       />
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -20,7 +20,7 @@ const Widget = () => {
     location.pathname === path ? "scale-125" : "";
 
   return (
-    <div className="absolute bottom-10 right-1/2 transform translate-x-1/2 bg-red-500/40 rounded-xl h-fit py-3 px-6 z-50">
+    <div className="fixed bottom-10 right-1/2 transform translate-x-1/2 bg-red-500/40 rounded-xl h-fit py-3 px-6 z-50">
       <div className="flex justify-center items-center gap-10">
         {navigationItems.map(({ path, icon: Icon }) => (
           <Link key={path} to={path}>
